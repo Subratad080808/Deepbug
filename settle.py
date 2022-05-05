517d3c5cbf7c358e45fef0c96c0f0ac7d159b811
Subrata <sdas18@lakeheadu.ca>
2022-05-05 15:00:06 -0400

    Learn Laravel

---
+++ settle.py
@@ -0,0 +1,22 @@
+from search import post_id_sim1
+
+from class9 import longi
+
+post_id_sim1(id1,ope1)
+
+longi().disk_persearch(uo,'254')
+
+
+class alpha_myo():
+	def process(conv,jar_size):
+		butter=jar_size-conv*jar_size
+		butter=longi().disk_persearch(selfi,butter)
+		return butter
+
+
+def set_process(self,round_number):
+	self.rex=round_number*23
+	self.porx=round_number*44
+	
+
+
